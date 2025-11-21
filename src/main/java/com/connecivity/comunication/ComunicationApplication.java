71e3e7e53a69f21434352f884423b08791d5337a
ujados <ale6alon@hotmail.com>
2025-11-21 19:19:10 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/connecivity/comunication/ComunicationApplication.java
@@ -0,0 +1,13 @@
+package com.connecivity.comunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connecivity/comunication/ComunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connecivity.comunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
